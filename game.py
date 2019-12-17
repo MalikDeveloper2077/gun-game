31f727c0c0fc87385b04249fc93fd90feb21dab4
MalikDeveloper2077 <nAsad1385@gmail.com>
2019-12-17 22:26:14 +0300

    Add the gun class and modify targets

--- game.py
+++ game.py
@@ -1,15 +1,63 @@
 from random import randint, choice
+import math
 import tkinter as tk
 
-WIDTH = 800
-HEIGHT = 600
-TARGET_COLORS = ['green', 'red', 'black', 'blue', 'pink', 'yellow']
+WIDTH = 1200
+HEIGHT = 800
+MAX_TARGETS = 15
+TARGET_COLORS = ['green', 'red', 'black', 'blue', 'pink', 'yellow', 'magenta']
+GUN_X = 40
+GUN_Y = HEIGHT // 3
+
+
+class Gun:
+    def __init__(self):
+        self.x = GUN_X
+        self.y = GUN_Y
+        self.power = 10
+        self.angle = 1
+        self.speed = randint(3, 5)
+        self.fire_on = False
+        self.gun_id = canvas.create_line(
+            self.x, self.y, 25, self.y, width=8
+        )
+
+    def move(self):
+        """ Moving the gun along y
+        Y of the gun increases on self.speed
+        """
+        self.y += self.speed
+
+        if self.y >= HEIGHT - 30 or self.y <= 30:
+            self.speed = -self.speed
+
+    def show(self):
+        """ Show gun move """
+        canvas.move(self.gun_id, 0, self.speed)
+
+    def aiming(self, event=0):
+        """ To aim at a target
+        Depends on mouse position
+        """
+        if event:
+            try:
+                self.angle = math.atan((event.y - self.y) / (event.x - self.x))
+            except ZeroDivisionError:
+                self.angle = math.atan((event.y - self.y) / 1)
+
+        if self.fire_on:
+            canvas.itemconfig(self.gun_id, fill="red")
+
+        canvas.coords(self.gun_id, self.x, self.y,
+                      self.x + max(self.power, self.x) * math.cos(self.angle),
+                      self.y + max(self.power, self.x) * math.sin(self.angle)
+                      )
 
 
 class Target:
     def __init__(self):
         self.radius = randint(15, 35)
-        self.x = randint(500, WIDTH - self.radius)
+        self.x = randint(WIDTH // 1.3, WIDTH - self.radius)
         self.y = randint(self.radius, HEIGHT - self.radius)
         self.speed_x = randint(7, 15)
         self.speed_y = randint(7, 15)
@@ -20,28 +68,29 @@ class Target:
         )
 
     def move(self):
-        """ Перемещение мяча за единицу времени
-        Метод увеличивает x и y снаряда, тем самым двигает его
-        на speed_x и speed_y за один кадр прорисовки
+        """ Moving the target every time which specified in tick()
+        The method increases the x and y on speed_x and
+        speed_y in one drawing frame
         """
         self.x += self.speed_x
         self.y += self.speed_y
 
-        if self.x + self.radius >= WIDTH or self.x - self.radius <= 500:
+        if self.x + self.radius >= WIDTH or self.x - self.radius <= WIDTH // 1.5:
             self.speed_x = -self.speed_x
 
         if self.y + self.radius >= HEIGHT or self.y - self.radius <= 0:
             self.speed_y = -self.speed_y
 
     def show(self):
-        """ Показ движения цели на холсте """
+        """ Shows target move """
         canvas.move(self.target_id, self.speed_x, self.speed_y)
 
 
 def tick():
-    """ Метод прорисовывает (обновляет) холст
-    За единицу времени - 50 милисекунд
-    """
+    """ Method updates the canvas every 50 ms """
+    gun.move()
+    gun.show()
+
     for target in targets:
         target.move()
         target.show()
@@ -50,14 +99,23 @@ def tick():
 
 
 def main():
-    global root, canvas, targets
+    """ App interface and settings
+    Define elements on canvas + binds
+    """
+    global root, canvas, gun, targets
 
     root = tk.Tk()
+    root.title("Gun Game")
     root.geometry(str(WIDTH) + "x" + str(HEIGHT))
     canvas = tk.Canvas(root)
     canvas.pack(fill=tk.BOTH, expand=1)
 
-    targets = [Target() for target in range(randint(2, 7))]
+    # All elements on canvas
+    gun = Gun()
+    targets = [Target() for target in range(randint(5, MAX_TARGETS))]
+
+    # Binds
+    canvas.bind('<Motion>', gun.aiming)
 
     tick()
     root.mainloop()
